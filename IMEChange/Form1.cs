23dc417eab0afa2a04e5756706ba77921eb3b419
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-01-21 10:55:37 +0900

    Change: Include Project Files

---
+++ IMEChange/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace IMEChange
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		protected override void OnLoad(EventArgs e)
+		{
+			SendKeys.Send("+(%)");
+			Close();
+		}
+	}
+}
